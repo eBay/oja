7ae883a7c8f36096ca8caf175e08ea36c3f66778
dsemenov <dsemenov@ebay.com>
2019-12-09 23:09:17 -0800

    added assertions of location action.json vs new action action.json

--- packages/hygen-oja-generators/__tests__/action.js
+++ packages/hygen-oja-generators/__tests__/action.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Assert = require('assert');
 const Fs = require('fs');
 const Path = require('path');
 const Shell = require('shelljs');
@@ -25,16 +26,57 @@ describe(__filename, () => {
 
         afterEach(() => {
             Shell.cd(cwd);
-            Shell.rm('-rf', `${tmpBase}`);
+            // Shell.rm('-rf', `${tmpBase}`);
         });
 
-        test('should add new page with method and security level', () => {
-            runCmd(`npm install ../../../../oja-context ../../../../oja-action`);
+        test('should add a set of actions', () => {
             runCmd(`hygen oja init`);
             runCmd(`hygen action new FOONS/foo`);
             runCmd(`hygen action new FOONS/bar`);
             runCmd(`hygen action new QAZNS/qaz --target src`);
             runCmd(`hygen action new SERVICES/svc1 --target src/service`);
+            runCmd(`npm install ../../../../oja-context ../../../../oja-action`);
+            runCmd('npm run test:actions');
+            runCmd('npm run test:actions:coverage');
+        });
+
+        test('should add a set of actions with actions location file is present', () => {
+            runCmd(`hygen oja init`);
+            runCmd(`hygen action new QAZNS/qaz --target src`);
+            runCmd(`hygen action new SERVICES/svc1 --target src/service`);
+            runCmd(`npm install ../../../../oja-context ../../../../oja-action`);
+            runCmd('npm run test:actions');
+            runCmd('npm run test:actions:coverage');
+        });
+
+        test('should add a set of actions, when scripts in package.json is not empty', () => {
+            Fs.writeFileSync(`${tmpDir}/package.json`, JSON.stringify({
+                name: 'app',
+                version: '1.0.0',
+                scripts: {
+                    test: 'echo node tests'
+                }
+            }, null, 2));
+            runCmd(`hygen oja init`);
+            runCmd(`hygen action new FOONS/foo`);
+            runCmd(`hygen action new FOONS/bar`);
+            runCmd(`hygen action new QAZNS/qaz --target src`);
+            runCmd(`hygen action new SERVICES/svc1 --target src/service`);
+            runCmd(`npm install ../../../../oja-context ../../../../oja-action`);
+            runCmd('npm run test:actions');
+            runCmd('npm run test:actions:coverage');
+        });
+
+        test('should fail to add a single action to the root when actions location file is present', () => {
+            runCmd(`hygen oja init`);
+            runCmd(`hygen action init`);
+            Assert.throws(() => {
+                runCmd(`hygen action new FOONS/foo`);
+            }, /Error: command hygen action new FOONS\/foo failed/);
+
+            runCmd(`hygen action new QAZNS/qaz --target src`);
+            runCmd(`hygen action new SERVICES/svc1 --target src/service`);
+            runCmd(`npm install ../../../../oja-context ../../../../oja-action`);
             runCmd('npm run test:actions');
             runCmd('npm run test:actions:coverage');
         });
