92a002cf3ff6836e994de9d7497a127a3ed919fb
dsemenov <dsemenov@ebay.com>
2019-12-30 19:28:49 -0800

    lint fixes

--- packages/examples/src/actions/itemDetails/index.js
+++ packages/examples/src/actions/itemDetails/index.js
@@ -6,23 +6,21 @@ const request = require('request');
  * Action: itemDetails
  * Domain: actions
  */
-module.exports = context => itemId => {
-    return new Promise((resolve, reject) => {
-        // use mock data, no item id needed for demo
-        request({
-            url: 'https://dimichgh.github.io/oja-selling-example/mock-data/item-details.json'
-        },
-        (err, response) => {
-            if (err) {
-                return reject(err);
-            }
-            if (response.statusCode >= 400) {
-                err = new Error(`Http Error, status:${response.statusCode}`);
-                err.statusCode = response.statusCode;
-                err.body = response.body.toString();
-                return reject(err);
-            }
-            resolve(JSON.parse(response.body));
-        });
+module.exports = context => itemId => new Promise((resolve, reject) => {
+    // use mock data, no item id needed for demo
+    request({
+        url: 'https://dimichgh.github.io/oja-selling-example/mock-data/item-details.json'
+    },
+    (err, response) => {
+        if (err) {
+            return reject(err);
+        }
+        if (response.statusCode >= 400) {
+            err = new Error(`Http Error, status:${response.statusCode}`);
+            err.statusCode = response.statusCode;
+            err.body = response.body.toString();
+            return reject(err);
+        }
+        resolve(JSON.parse(response.body));
     });
-};
+});
